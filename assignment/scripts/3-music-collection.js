51c4d94eeec1cf93d087a33b12fa082257363a90
Lily LaCosse <lilylacosse@gmail.com>
2023-02-15 16:42:05 -0600

    completed addToCollection - need to explore why my object does not have a name in my array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,5 +3,14 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection(title, artist, yearPublished) { 
-    
+    let album = {
+        'title': title,
+        'artist': artist,
+        'yearPublished': yearPublished
+    }
+    collection.push(album);
+    return console.log(album);
 }
+
+addToCollection('Revolver', 'The Beatles', 1966);
+console.log(collection);
