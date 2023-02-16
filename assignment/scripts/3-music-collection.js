cef537491e79df2d02453876646ee75a47938c1d
Lily LaCosse <lilylacosse@gmail.com>
2023-02-16 09:05:17 -0600

    Tested addToCollection, array is working properly

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,16 +1,23 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+beatlesDiscography = [];
 
-function addToCollection(title, artist, yearPublished) { 
-    let album = {
+function addToCollection( title, artist, yearPublished) { 
+   let album = {
         'title': title,
         'artist': artist,
         'yearPublished': yearPublished
     }
     collection.push(album);
-    return console.log(album);
+    return console.log('Album Added:',album);
 }
 
 addToCollection('Revolver', 'The Beatles', 1966);
-console.log(collection);
+addToCollection('The White Album', 'The Beatles', 1968);
+addToCollection('Icky Thump', 'The White Stripes', 2007);
+addToCollection('Blonde on Blonde', 'Bob Dylan', 1966);
+addToCollection('Blood on the Tracks', 'Bob Dylan', 1975);
+addToCollection('White Blood Cells', 'The White Stripes', 2001);
+
+console.log('Albums in Colleciton:', collection);
